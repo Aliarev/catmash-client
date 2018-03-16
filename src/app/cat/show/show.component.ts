078c732884449938e7677190795999a02db70c12
Antoine LAVAIRE <antoine.lavaire@netapsys.fr>
2018-03-16 12:31:36 +0100

    Ajout du classement

--- src/app/cat/show/show.component.ts
+++ src/app/cat/show/show.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CatDto, WebApiService } from '../../service/webapi.service';
+
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html'
 })
 export class CatShowComponent implements OnInit {
-  public constructor() { }
+  public cats: CatDto[] = [];
+
+  public constructor(
+    private webapiService: WebApiService
+  ) { }
 
   public ngOnInit() {
+    this.webapiService.apiCatAllGet().subscribe(cats => this.cats = cats);
   }
 }
