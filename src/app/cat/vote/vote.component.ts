9f0f5376ed2e07db89445e47dda4e19ac739a29b
Antoine LAVAIRE <antoine.lavaire@netapsys.fr>
2018-03-17 22:49:16 +0100

    Ajout du vote

--- src/app/cat/vote/vote.component.ts
+++ src/app/cat/vote/vote.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CatDto, VoteDto, WebApiService } from 'app/service/webapi.service';
+
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html'
 })
 export class CatVoteComponent implements OnInit {
-  public constructor() { }
+  public cats: CatDto[] = [];
+  public loading: boolean = true;
+
+
+  public constructor(
+    private webApiService: WebApiService
+  ) { }
 
   public ngOnInit() {
+    this.getNextCats();
+  }
+
+  private getNextCats() {
+    this.loading = true;
+    this.webApiService.apiCatNextGet().subscribe(
+      cats => {
+        this.cats = cats;
+        this.loading = false;
+      },
+      err => this.loading = false
+    );
+  }
+
+  public vote(indexUp: number) {
+    const vote = new VoteDto({
+      idDown: this.cats[(indexUp + 1) % 2].id,
+      idUp: this.cats[indexUp].id
+    });
+    this.webApiService.apiCatVotePost(vote).subscribe(() => this.getNextCats());
   }
 }
